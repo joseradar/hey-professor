25d69f0890a47a8e1e064d1f1c91b60d79aaef88
jose <jose.radar@hotmail.com>
2024-04-22 16:38:04 -0300

    HEY-27: it should order by like and unlike, most liked question should be at the top, most unliked questions should be in the bottom

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,13 @@ class DashboardController extends Controller
 {
     public function __invoke(): View
     {
-        $questions = Question::withSum('votes', 'like')->withSum('votes', 'unlike')->paginate(7);
+        $questions = Question::withSum('votes', 'like')
+            ->withSum('votes', 'unlike')
+            ->orderByRaw('
+                    case when votes_sum_like is null then 0 else votes_sum_like end desc,
+                    case when votes_sum_unlike is null then 0 else votes_sum_unlike end
+                ')
+            ->paginate(5);
 
         return view('dashboard', compact('questions'));
     }
