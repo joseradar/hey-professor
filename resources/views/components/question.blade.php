264549607045dafe4351fb00c13bb014f10d7767
jose <jose.radar@hotmail.com>
2024-04-21 23:40:39 -0300

    HEY-16: refactor to perform

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -10,7 +10,7 @@
             <button type="submit" class="flex items-center space-x-2 text-green-500 cursor-pointer">
                 <x-icons.thumbs-up class=" w-5 h-5 hover:text-green-300 " id="thumbs-up"/>
                 <span>
-                    {{ $question->votes->where('like', 1)->count()}}
+                    {{ $question->votes_sum_like ?: 0 }}
                 </span>
 
             </button>
@@ -19,7 +19,7 @@
             <button type="submit" class="flex items-center space-x-2 text-red-500 cursor-pointer">
                 <x-icons.thumbs-down class="w-5 h-5 hover:text-red-300 " id="thumbs-down"/>
                 <span>
-                {{ $question->votes->where('unlike', 1)->count()}}
+                {{ $question->votes_sum_unlike ?: 0}}
             </span>
             </button>
         </x-form>
