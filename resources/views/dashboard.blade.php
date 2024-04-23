ed0a9d4db816cbac091a7de99b39d25172ec9f2a
jose <jose.radar@hotmail.com>
2024-04-23 10:41:11 -0300

    HEY-19: aplying filter question in the front end

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,11 +10,28 @@
 
     <x-container>
         <div class="flex flex-col space-y-4">
-            @foreach ($questions as $item)
-                <x-question :question="$item"/>
-            @endforeach
 
-            {{$questions->withQueryString()->links()}}
+            <x-form :action="route('dashboard')" get>
+                <x-inputs.search autocomplete="off"/>
+            </x-form>
+
+            @if($questions->isEmpty())
+                <div class="dark:text-gray-300 text-center flex flex-col justify-center">
+                    <p class="text-2xl font-bold mb-8 mt-5">No questions found</p>
+                    <div class="flex justify-center">
+                        <x-draws.searching width="350"/>
+                    </div>
+
+
+                </div>
+            @else
+
+                @foreach ($questions as $item)
+                    <x-question :question="$item"/>
+                @endforeach
+
+                {{$questions->withQueryString()->links()}}
+            @endif
         </div>
     </x-container>
 
