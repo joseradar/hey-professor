55083719261d2635e9cb4aed63fd95f6d39fbb52
jose <jose.radar@hotmail.com>
2024-04-22 22:32:03 -0300

    HEY-18: remove trashcodes

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\{Artisan, Schedule};
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+use Illuminate\Support\Facades\{Schedule};
 
 Schedule::command('model:prune')->daily();
